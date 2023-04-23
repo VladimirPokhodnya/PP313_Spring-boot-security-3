f4c0ec90ab9992dfddef2e0adbd779c77eeec5a5
hammondy <hammondy@student.21-school.ru>
2023-04-23 13:54:48 +0700

    models, repositories

---
+++ src/main/java/ru/kata/spring/boot_security/demo/models/User.java
@@ -0,0 +1,56 @@
+package ru.kata.spring.boot_security.demo.models;
+
+import lombok.Data;
+import lombok.NonNull;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Byte age;
+    private String username;
+    private String surname;
+    private String password;
+    @Column(unique=true)
+    private String email;
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Collection<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getAuthority())).collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,17 @@
-INSERT INTO pp313.users (age, email, first_name, last_name, password)
+use pp313;
+
+
+INSERT INTO pp313.users (age, email, username, surname, password)
 VALUES (33, 'admin@mail.com', 'admin', 'root', '$2a$10$OYJj3pfcLsrPKxqoe7/0teaPEMC0pKF3iwUgoHoLcSG9j8YG4asSe');
 
-INSERT INTO pp313.roles (role)
+INSERT INTO pp313.users (age, email, username, surname, password)
+VALUES (33, 'user@mail.com', 'user', 'user', '$2a$10$OYJj3pfcLsrPKxqoe7/0teaPEMC0pKF3iwUgoHoLcSG9j8YG4asSe');
+
+
+INSERT INTO pp313.roles (name)
 VALUES ('USER');
 
-INSERT INTO pp313.roles (role)
+INSERT INTO pp313.roles (name)
 VALUES ('ADMIN');
 
 INSERT INTO pp313.users_roles (user_id, role_id)
@@ -12,3 +19,6 @@ VALUES (1, 1);
 
 INSERT INTO pp313.users_roles (user_id, role_id)
 VALUES (1, 2);
+
+INSERT INTO pp313.users_roles (user_id, role_id)
+VALUES (2, 1);
